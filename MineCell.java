b0fc8ec3df3448977795f747ff8d1e7a2be19df9
jtaejune1159 <62458970+jtaejune1159@users.noreply.github.com>
2020-12-03 16:00:52 +0900

    Update MineCell.java

--- MineCell.java
+++ MineCell.java
@@ -1,7 +1,7 @@
 public class MineCell {
     private boolean ismine = false;
     private int neighbor_count = 0; // 주의의 지뢰의 개수를 저장하는 변수, 9이면 지뢰로 간주
-    private String face_value;
+    private String face_value; // 
     
     public MineCell(String value){
         face_value = value;
@@ -11,4 +11,5 @@ public class MineCell {
     public boolean getMine() { return ismine; }
     public void setNeighbor_count(int n) { neighbor_count = n; }
     public int getNeighbor_count() { return neighbor_count; }
+    public boolean getflag() { return face_value == '#'; } // 만약 플래스가 세워져있으면 true, 아니면 false;
 }
