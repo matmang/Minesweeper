995cda5cdbb3fae7eff67fe635447372f4d22cef
jtaejune1159 <62458970+jtaejune1159@users.noreply.github.com>
2020-12-07 17:00:08 +0900

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Board {
     private int size;
     private int mine_cnt;
@@ -13,16 +15,29 @@ public class Board {
         	mine_cnt = 90;
         }
         board = new MineCell[size][size];
-        
-        
-        for(int i = 0; i != size; i = i + 1) {
-        	for(int j = 0; j != size; j = j + 1) {
-        		answer[i][j] = board[i][j];
+        for(int i=0; i<size; i++) {
+        	for(int j=0; j<size; j++) {
+        		board[i][j] = new MineCell("");
         	}
         }
-        
+        Laymine(board);
+        minerule(board);
     }
-    
+    private void Laymine(MineCell[][] board){
+    	MineCell t = new MineCell("");
+    	MineCell f = new MineCell("");
+    	t.setMine(true);
+    	f.setMine(false);
+    	Random ran = new Random();
+        while (mine_cnt > 0){
+            int row = ran.nextInt(size);
+            int col = ran.nextInt(size);
+            if(!board[row][col].getMine()){
+                mine_cnt--;
+                board[row][col].setMine(true);
+            }
+        }
+    }
     
     public MineCell[][] answerboard() {
         MineCell[][] answer = new MineCell[size][size];
@@ -32,7 +47,22 @@ public class Board {
         return answer;
     }
     
-    public MineCell[][] gameboard(){
-    	
+    private void minerule(MineCell[][] c) {
+    	int mine=0;
+        int[] x = {-1, 0, 1, 1, 1, 0, -1, -1};
+        int[] y = {-1, -1, -1, 0, 1, 1, 1, 0};
+        for(int i=0; i<size ; i++) {
+        	for(int j=0; j<size; j++) {
+        		mine = 0;
+        		for(int k=0; k<8; k++) {
+        			if(0<= i+y[k] && i+y[k] < size && 0 <= j+x[k] && j+x[k] < size
+        					&& c[i+y[k]][j+x[k]].getMine())
+        				mine++;
+        		}
+        		if(!c[i][j].getMine())
+        			c[i][j].setNeighbor_count(mine);
+        	}
+        }
     }
+    
 }
