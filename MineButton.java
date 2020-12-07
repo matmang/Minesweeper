b231bb58fa37927bc25402c72250ccaed83fbb1e
jtaejune1159 <62458970+jtaejune1159@users.noreply.github.com>
2020-12-07 17:25:06 +0900

    Update MineButton.java

--- MineButton.java
+++ MineButton.java
@@ -6,14 +6,22 @@ import javax.swing.JButton;
 public class MineButton extends JButton implements ActionListener{
 	private Board puzzle;
 	private MineFrame view;
+	private MineCell cell;
 	
 	public MineButton(Board p, MineFrame v) {
 		puzzle = p;
 		view = v;
 		addActionListener(this);
 	}
+	public void setMineCell(MineCell c) {
+		cell = c;
+	}
 	
 	public void actionPerformed(ActionEvent e) {
-		
+		if(cell.getMine()) {
+			setText("M");
+		}else {
+			setText("" + cell.getNeighbor_count());
+		}
 	}
 }
