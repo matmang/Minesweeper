f0454d0a2f02b43c60ce594d9407c577ebc157ab
jtaejune1159 <62458970+jtaejune1159@users.noreply.github.com>
2020-12-07 15:43:09 +0900

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,25 +1,38 @@
 public class Board {
     private int size;
+    private int mine_cnt;
     private MineCell[][] board;
 
     public Board (int s) {
         size = s;
+        if(s == 9) {
+        	mine_cnt = 10;
+        }else if(s == 16) {
+        	mine_cnt = 40;
+        }else if(s == 22) {
+        	mine_cnt = 90;
+        }
         board = new MineCell[size][size];
         
-        for (int num = 1; num != size * size; num = num + 1) { 
-        	MineCell p = new MineCell();
-            int row = num / size;
-            int col = num % size;
-            board[row][col] = p;
+        
+        for(int i = 0; i != size; i = i + 1) {
+        	for(int j = 0; j != size; j = j + 1) {
+        		answer[i][j] = board[i][j];
+        	}
         }
+        
     }
     
     
-    public MineCell[][] returnboard() { 
+    public MineCell[][] answerboard() {
         MineCell[][] answer = new MineCell[size][size];
         for(int i = 0; i != size; i = i + 1)
         	for(int j = 0; j != size; j = j + 1)
         		answer[i][j] = board[i][j];
-            return answer;
+        return answer;
+    }
+    
+    public MineCell[][] gameboard(){
+    	
     }
 }
